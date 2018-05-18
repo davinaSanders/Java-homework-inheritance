6d848ab7aa78d9eb71727bfbf46cbd3e1817afcc
davina <davina.kuhnel@gmail.com>
2018-05-18 16:39:41 +0100

    created getters for Booking class

--- src/main/java/Booking.java
+++ src/main/java/Booking.java
@@ -2,4 +2,25 @@ public class Booking {
     private Room room;
     private Guest guest;
     private int nights;
+
+    public Booking(Room room, Guest guest, int nights){
+        this.room = room;
+        this.guest = guest;
+        this.nights = nights;
+
+    }
+
+
+    public int getnights(){
+        return this.nights;
+    }
+
+
+    public Room getroom(){
+        return this.room;
+    }
+
+    public Guest getguest(){
+        return this.guest;
+    }
 }
